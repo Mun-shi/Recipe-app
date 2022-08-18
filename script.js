346f55f2d64265e5afc70c03a6b6824825d2e4d9
Mun-shi <90496225+Mun-shi@users.noreply.github.com>
2022-08-18 12:19:52 +0530

    update

---
+++ script.js
@@ -0,0 +1,11 @@
+function getRandomMeal(){
+    fetch("www.themealdb.com/api/json/v1/1/random.php")
+}
+
+function getMealById(id){
+    fetch("www.themealdb.com/api/json/v1/1/lookup.php?i=52772")
+}
+
+function getMealBySearch(term){
+
+}
