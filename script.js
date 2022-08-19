35f7239d9bbfd6579da90607ce3dfb7d83a3df06
Mun-shi <90496225+Mun-shi@users.noreply.github.com>
2022-08-19 16:24:35 +0530

    update

--- script.js
+++ script.js
@@ -1,11 +1,43 @@
-function getRandomMeal(){
-    fetch("www.themealdb.com/api/json/v1/1/random.php")
+const meals = document.getElementById("meals");
+
+getRandomMeal();
+
+async function getRandomMeal(){
+    const resp = await fetch("https://www.themealdb.com/api/json/v1/1/random.php");
+    const respData = await resp.json();
+    const randomMeal = respData.meals[0];
+
+    addMeal(randomMeal, true);
 }
 
-function getMealById(id){
-    fetch("www.themealdb.com/api/json/v1/1/lookup.php?i=52772")
+async function getMealById(id){
+    const meal = await fetch("https://www.themealdb.com/api/json/v1/1/lookup.php?i="+ id);
 }
 
-function getMealBySearch(term){
-
+async function getMealBySearch(term){
+    const meals = await fetch("https://www.themealdb.com/api/json/v1/1/search.php?s=" + term);
 }
+addMeal(mealData, random = false){
+  const meal = document.createElement('div');
+  meal.classList.add('meal');
+
+  meal.innerHTML = `
+    
+  <div class ="meal-header">
+  ${random ? `
+  <span class="random">
+  Random Recipe
+</span>`: '' }
+  
+<img src="${mealData.strMealThumb}" alt=""/>
+</div>
+<div class="meal-body">
+  <h4>Veggie veggies</h4>
+  <button class="fav-btn">
+      <i class="fa fa-heart"></i>
+  </button>
+</div> 
+  `
+}
+
+ 
